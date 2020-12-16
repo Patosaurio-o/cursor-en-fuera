af0fcd114273842bcb8ad27e1217b589d00867f5
Patosaurio_o <pato.sepulveda@hotmail.es>
2020-12-16 09:43:17 -0300

    finish him

---
+++ code.js
@@ -0,0 +1,30 @@
+$('img').on('click', function(){
+  $(this).slideUp();
+  decir('ouch');
+});
+
+$('#reset').on('click', function(){
+  location.reload();
+  decir('im back');
+});
+
+function decir(mensaje){
+  var msg = new SpeechSynthesisUtterance();
+  msg.text = mensaje;
+  window.speechSynthesis.speak(msg)
+};
+
+function cambioImagen(){
+  var img1 = $(this).attr('src');
+  var img2 = $(this).attr('data-src');
+  $(this).attr('src', img2);
+  $(this).attr('data-src', img1);
+};
+
+$("img").hover(cambioImagen,cambioImagen);
+$("img").hover(function(){
+  $(this).css('transform', 'scale(1.2)');
+},
+function(){
+  $(this).css('transform', 'scale(1)');
+});
